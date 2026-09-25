030b188af1f030087638cc8f86ed796cb77ef50f
Veronika Litvinenko <korynaitroyd@gmail.com>
2026-09-25 13:12:38 +0300

    Update MoneyTransferTest.java

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web.test;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashBoardPage;
@@ -19,7 +18,6 @@ class MoneyTransferTest {
 
     @BeforeEach
     void setup() {
-        Configuration.holdBrowserOpen = true;
         open("http://127.0.0.1:9999");
 
         var authInfo = DataHelper.getAutoInfo();
@@ -34,12 +32,8 @@ class MoneyTransferTest {
         initialBalance2 = dashboardPage.getCardBalance(secondCard);
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
+    // Перевод денег с первой карты на вторую
     @Test
-// Перевод денег с первой карты на вторую
     void shouldTransferMoneyFromFirstCardToSecond() {
         int amount = Math.max(1, initialBalance1 / 4);
 
@@ -50,8 +44,8 @@ class MoneyTransferTest {
         assertEquals(initialBalance2 + amount, dashboardPage.getCardBalance(secondCard));
     }
 
+    // Перевод денег со второй карты на первую
     @Test
-// Перевод денег со второй карты на первую
     void shouldTransferMoneyFromSecondCardToFirst() {
         int amount = Math.max(1, initialBalance2 / 4);
 
@@ -62,8 +56,9 @@ class MoneyTransferTest {
         assertEquals(initialBalance2 - amount, dashboardPage.getCardBalance(secondCard));
     }
 
+    // Перевод суммы больше остатка
+    @Disabled("Баг #1: перевод сверх баланса выполняется, баланс уходит в минус")
     @Test
-        // Перевод суммы больше остатка
     void shouldGetErrorWhenTransferringMoreThanAvailable() {
         int amount = initialBalance1 + 1; // на 1 рубль больше, чем есть
         var transferPage = dashboardPage.selectCard(secondCard);
@@ -71,8 +66,9 @@ class MoneyTransferTest {
         transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
     }
 
+    // Перевод отклоняется из-за превышения лимита средств, а баланс остаётся тем же
+    @Disabled("Баг #2: перевод сверх баланса выполняется, баланс уходит в минус")
     @Test
-        // Перевод отклоняется из-за превышения лимита средств, а баланс остаётся тем же
     void shouldKeepBalanceUnchangedAfterFailedTransfer() {
         int amount = initialBalance1 + 500; // заведомо больше, чем доступно
 
@@ -85,8 +81,8 @@ class MoneyTransferTest {
         assertEquals(initialBalance2, dashboardPage.getCardBalance(secondCard));
     }
 
+    // Перевод 0 рублей
     @Test
-        // Перевод 0 рублей
     void shouldTransferZeroAmount() {
         var transferPage = dashboardPage.selectCard(secondCard);
         dashboardPage = transferPage.makeValidTransfer("0", firstCard);
